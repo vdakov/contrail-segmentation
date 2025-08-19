e60df8a1bfea9f7599a85c0bf3c6fe3d78ab897f
vdakov <v.dakov02@gmail.com>
2025-08-19 10:31:18 +0300

    Adjust visualization of images from MNIST

--- visualization/show_images.py
+++ visualization/show_images.py
@@ -5,6 +5,7 @@ def visualize_pytorch_dataset_images(trainset, title):
     fig, axs = plt.subplots(1, 10, figsize=(15, 3))
     for i in range(10):
         img, label = trainset[i]
+        img = img.cpu().numpy().squeeze()  # Convert to numpy and remove channel dimension if present
         axs[i].imshow(img.squeeze(), cmap='gray')
         axs[i].set_title(f'Label: {label}')
         axs[i].axis('off')

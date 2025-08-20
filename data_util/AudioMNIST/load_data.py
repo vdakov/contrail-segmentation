8d4fb0b5f60a034f1d724da9f8bd7fc402e362e2
vdakov <v.dakov02@gmail.com>
2025-08-20 18:06:46 +0300

    Added sample questions

--- data_util/AudioMNIST/load_data.py
+++ data_util/AudioMNIST/load_data.py
@@ -17,13 +17,13 @@ def load_audio_data(folder_path, max_samples=3000):
     
     return X, y, wavs
 
-def load_audio_data_noisy(folder_path, max_samples=3000):
+def load_audio_data_noisy(folder_path, augmentation_function, max_samples=3000):
     X, y, wavs = [], [], []
     for file in tqdm(os.listdir(folder_path)[:max_samples]):
         x = librosa.load(os.path.join(folder_path, file), sr=44100)[0]
         wavs.append(x)
         X.append(preprocess_wav_into_features(np.array(x)))
-        x_noisy = x + np.random.normal(0, 0.001, x.shape)
+        x_noisy = augmentation_function(x)
         
         wavs.append(x_noisy)
         x = np.array(x_noisy)
